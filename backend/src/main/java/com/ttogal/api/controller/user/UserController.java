b9c71c425091e92da40dea3b53e460bf41268f9f
JeongIna <jeongina89@gmail.com>
2024-12-02 18:23:35 +0900

    [Feat] UserController에 이메일 및 닉네임 중복성 검사 및  회원가입 로직 구현

--- backend/src/main/java/com/ttogal/api/controller/user/UserController.java
+++ backend/src/main/java/com/ttogal/api/controller/user/UserController.java
@@ -1,6 +1,9 @@
 package com.ttogal.api.controller.user;
 
 import com.ttogal.api.controller.user.dto.request.UserRegisterRequestDto;
+import com.ttogal.api.controller.user.dto.request.ValidateEmailRequestDto;
+import com.ttogal.api.controller.user.dto.response.UserRegisterResponseDto;
+import com.ttogal.api.controller.user.dto.response.UserResponseDto;
 import com.ttogal.api.service.user.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
@@ -15,10 +18,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/users")
 public class UserController {
-private final UserService userService;
+  private final UserService userService;
 
-@PostMapping("/register")
-public ResponseEntity<Void> register(@RequestBody @Valid UserRegisterRequestDto dto) {
-  return new ResponseEntity<>(HttpStatus.OK);
-}
+  @PostMapping("/register")
+  public ResponseEntity<UserRegisterResponseDto> register(@RequestBody @Valid UserRegisterRequestDto dto) {
+    Long userId=userService.register(dto);
+    UserRegisterResponseDto responseDto=UserRegisterResponseDto.builder()
+            .userId(userId)
+            .message("회원가입이 성공적으로 완료되었습니다.")
+            .build();
+    return new ResponseEntity<>(responseDto,HttpStatus.OK);
+  }
+
+  @PostMapping("/validate-email")
+  public ResponseEntity<UserResponseDto> validateEmail(@RequestBody @Valid ValidateEmailRequestDto requestdto) {
+    userService.validateEmail(requestdto.email());
+    UserResponseDto responseDto=UserResponseDto.builder().message("이메일 검증이 완료되었습니다.").build();
+    return new ResponseEntity<>(responseDto,HttpStatus.OK);
+  }
+
+  @PostMapping("/validate-nickname")
+  public ResponseEntity<UserResponseDto> validateNickname(@RequestBody @Valid UserRegisterRequestDto requestdto) {
+    userService.validateNickname(requestdto.nickname());
+    UserResponseDto responseDto=UserResponseDto.builder().message("닉네임 검증이 완료되었습니다.").build();
+    return new ResponseEntity<>(responseDto,HttpStatus.OK);
+  }
 }
