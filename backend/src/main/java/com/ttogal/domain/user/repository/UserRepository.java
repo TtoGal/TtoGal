d4a16df6a39c72c459d38c90c8c2df6235ff0ed1
JeongIna <jeongina89@gmail.com>
2024-12-02 18:25:40 +0900

    [Feat] UserRepository에 검증 메소드 구현

--- backend/src/main/java/com/ttogal/domain/user/repository/UserRepository.java
+++ backend/src/main/java/com/ttogal/domain/user/repository/UserRepository.java
@@ -2,6 +2,11 @@ package com.ttogal.domain.user.repository;
 
 import com.ttogal.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+  boolean existsByEmail(String email);
+
+  boolean existsByNickname(String nickname);
 }
