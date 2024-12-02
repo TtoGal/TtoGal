e1c39b7115ad148cf3d0f148bc97ac38c4deb62d
JeongIna <jeongina89@gmail.com>
2024-12-02 18:22:31 +0900

    [Feat] 스웨거 및 h2 확인 경로에 대해서 filter처리하지 않도록 코드 추가

--- backend/src/main/java/com/ttogal/common/config/SecurityConfig.java
+++ backend/src/main/java/com/ttogal/common/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -34,5 +35,16 @@ public class SecurityConfig {
                     .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
     return http.build();
+  }
+
+  @Bean
+  public WebSecurityCustomizer webSecurityCustomizer() {
+    return web -> web.ignoring().requestMatchers("/v3/api-docs/**",         // OpenAPI 문서 경로
+            "/swagger-ui/**",          // Swagger UI 경로
+            "/swagger-ui/index.html",  // Swagger UI 인덱스
+            "/webjars/**",
+            "/index.html",
+            "/h2-console/**"
+    );// Swagger UI에 필요한 웹 자원와 어플리케이션 정적 리소스)
   }
 }
