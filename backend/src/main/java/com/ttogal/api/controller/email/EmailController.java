e2b1f182ccb10b740cf39d1f7f3b4c21fed6a742
JeongIna <jeongina89@gmail.com>
2024-12-03 13:27:48 +0900

    [Feat] EmailController 내 메소드 반환형 ResponseEntity<>로 통일

--- backend/src/main/java/com/ttogal/api/controller/email/EmailController.java
+++ backend/src/main/java/com/ttogal/api/controller/email/EmailController.java
@@ -1,9 +1,11 @@
 package com.ttogal.api.controller.email;
 
-import com.ttogal.api.service.EmailService;
-import com.ttogal.domain.email.dto.request.EmailRequest;
-import com.ttogal.domain.email.dto.request.VerifyCodeRequest;
+import com.ttogal.api.service.email.EmailService;
+import com.ttogal.api.controller.email.dto.request.EmailRequestDto;
+import com.ttogal.api.controller.email.dto.request.VerifyCodeRequestDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,16 +14,19 @@ import org.springframework.web.bind.annotation.*;
 public class EmailController {
     private final EmailService emailService;
 
-    @ResponseBody
     @PostMapping("/send")
-    public String emailCheck(@RequestBody EmailRequest request) {
-        return emailService.sendSimpleMessage(request.getEmail());
+    public ResponseEntity<String> emailCheck(@RequestBody EmailRequestDto request) {
+        String simpleMessage=emailService.sendSimpleMessage(request.getEmail());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(simpleMessage);
     }
 
     // 인증코드 인증
     @PostMapping("/verify")
-    public boolean verify(@RequestBody VerifyCodeRequest request) {
-        return emailService.verifyEmailCode(request.getEmail(), request.getAuthCode());
+    public ResponseEntity<Boolean> verify(@RequestBody VerifyCodeRequestDto request) {
+        Boolean isVerified=emailService.verifyEmailCode(request.getEmail(), request.getAuthCode());
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(isVerified);
     }
 
 
