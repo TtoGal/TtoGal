102014079a83d8daf1f20dda4020b1458eea7908
JeongIna <jeongina89@gmail.com>
2024-12-06 18:53:45 +0900

    [Feat] ValidateEmailRequestDto에 스웨거 적용

--- backend/src/main/java/com/ttogal/api/controller/user/dto/request/ValidateEmailRequestDto.java
+++ backend/src/main/java/com/ttogal/api/controller/user/dto/request/ValidateEmailRequestDto.java
@@ -1,11 +1,13 @@
 package com.ttogal.api.controller.user.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotNull;
 import lombok.Builder;
 
 @Builder
 public record ValidateEmailRequestDto(
+        @Schema(description = "사용자의 이메일 주소", example = "example@naver.com")
         @NotNull(message = "이메일 주소를 입력해주세요.")
         @Email(message = "유효한 이메일 형식이 아닙니다.")
         String email
