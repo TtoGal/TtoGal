20de0b0559f461516b29f8f6e280df9acf7e13f7
JeongIna <jeongina89@gmail.com>
2024-12-10 12:42:43 +0900

    [Feat] 인증 성공 또는 실패 시 적절히 응답해주는 메소드 개발(successfulAuthentication,unsuccessfulAuthentication )

--- backend/src/main/java/com/ttogal/common/filter/LoginFilter.java
+++ backend/src/main/java/com/ttogal/common/filter/LoginFilter.java
@@ -2,6 +2,8 @@ package com.ttogal.common.filter;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ttogal.api.controller.user.dto.request.LoginRequestDto;
+import com.ttogal.api.controller.user.dto.response.CustomUserDetails;
+import com.ttogal.common.util.JwtUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
@@ -10,10 +12,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Iterator;
 
 
 @Slf4j
@@ -21,31 +26,55 @@ public class LoginFilter extends AbstractAuthenticationProcessingFilter {
   private final static String REQUEST_URL = "/api/v1/users/login";
   private final static String HTTP_METHOD = "POST";
 
-  private static final AntPathRequestMatcher PATH_REQUEST_MATCHER = new AntPathRequestMatcher(REQUEST_URL,HTTP_METHOD);
+  private static final AntPathRequestMatcher PATH_REQUEST_MATCHER = new AntPathRequestMatcher(REQUEST_URL, HTTP_METHOD);
 
   private final ObjectMapper objectMapper;
+  private final JwtUtil jwtUtil;
 
-  public LoginFilter(ObjectMapper objectMapper) {
+  public LoginFilter(ObjectMapper objectMapper, JwtUtil jwtUtil) {
     super(PATH_REQUEST_MATCHER);
     this.objectMapper = objectMapper;
+    this.jwtUtil = jwtUtil;
   }
 
   @Override
   public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException, IOException {
-      LoginRequestDto dto=objectMapper.readValue(request.getInputStream(),
-              LoginRequestDto.class);
+    LoginRequestDto dto = objectMapper.readValue(request.getInputStream(),
+            LoginRequestDto.class);
     log.info("인증 시도 유저:{}", dto.email());
-      UsernamePasswordAuthenticationToken authenticationToken=new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
-      return this.getAuthenticationManager().authenticate(authenticationToken);
+    UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(dto.email(), dto.password());
+    return this.getAuthenticationManager().authenticate(authenticationToken);
   }
 
   @Override
-  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-    System.out.println("success");
+  protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authentication) throws IOException, ServletException {
+    String username = extractUsername(authentication);
+    String role = extractRole(authentication);
+
+    String accessToken = jwtUtil.createAccessToken(username, role);
+    String refrestToken = jwtUtil.createRefreshToken(username, role);
+
+    jwtUtil.sendAccessAndRefreshToken(response, accessToken, refrestToken);
+
+    log.info("로그인 성공: {}", username);
+    log.info("Access & refresh 토큰 생성");
   }
 
   @Override
   protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-    System.out.println("fail");
+    response.setStatus(401);
+  }
+
+  private String extractUsername(Authentication authentication) {
+    CustomUserDetails customUserDetails = (CustomUserDetails) authentication.getPrincipal();
+    return customUserDetails.getUsername();
+  }
+
+  private String extractRole(Authentication authentication) {
+    Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+    Iterator<? extends GrantedAuthority> iterator = authorities.iterator();
+    GrantedAuthority auth = iterator.next();
+    String role = auth.getAuthority();
+    return role;
   }
 }
